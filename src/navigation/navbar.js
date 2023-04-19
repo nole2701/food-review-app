8b78fedf0cbbcaf557b376ab028b44181f0e23af
bkornusov <batyrkornusov@gmail.com>
2023-04-19 03:00:16 -0400

    added bootstrap

---
+++ src/navigation/navbar.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+const Navbar = ({ active }) => {
+  return (
+    <div>
+      <div clasName="list-group"></div>
+    </div>
+  );
+};
+
+export default Navbar;
