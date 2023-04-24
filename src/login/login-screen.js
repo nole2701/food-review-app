d0d674e4558ef935728a46111a6ea33835591c21
nole2701 <nole2701@gmail.com>
2023-04-24 01:36:25 -0400

    uncommented some code

--- src/login/login-screen.js
+++ src/login/login-screen.js
@@ -24,8 +24,8 @@ function LoginScreen() {
         <input
           className="form-control"
           type="text"
-          // value={username}
-          // onChange={(event) => setUsername(event.target.value)}
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
         />
       </div>
       <div>
@@ -33,8 +33,8 @@ function LoginScreen() {
         <input
           className="form-control"
           type="password"
-          // value={password}
-          // onChange={(event) => setPassword(event.target.value)}
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
         />
       </div>
       <button onClick={handleLogin}>Login</button>
